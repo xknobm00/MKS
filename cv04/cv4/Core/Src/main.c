8ae83836a0ee62bf287ba07ada65f115679eff7c
xknobm00 <xknobm00@vutbr.cz>
2023-10-18 18:05:26 +0200

    přidán LEDbar

--- cv04/cv4/Core/Src/main.c
+++ cv04/cv4/Core/Src/main.c
@@ -112,7 +112,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  sct_value(raw_pot*(500+1)/4096);
+	  sct_value(raw_pot*(500+1)/4096, raw_pot*(8+1)/4096);
 	  HAL_Delay(50);
   }
   /* USER CODE END 3 */
